7c62cf4ce418500cbd0167319acb46461d0429d8
Rabi Ahmad <rabi.ahmad@capgemini.com>
2026-08-06 15:46:36 +0000

    line numbers and yank highlighting in neovim config

--- configs/nvim/lua/main/core/options.lua
+++ configs/nvim/lua/main/core/options.lua
@@ -12,43 +12,21 @@
 -- and general editor settings in this file.
 
 -- use system clipboard for all yank, delete, change and paste operations.
--- use windows clipboard (calls powershell).
-vim.g.clipboard = {
-    name = "WindowsClipboard",
-    copy = {
-        ["+"] = "clip.exe",
-        ["*"] = "clip.exe",
-    },
-    paste = {
-        ["+"] = {
-            "powershell.exe",
-            "-NoProfile",
-            "-Command",
-            "Get-Clipboard",
-        },
-        ["*"] = {
-            "powershell.exe",
-            "-NoProfile",
-            "-Command",
-            "Get-Clipboard",
-        },
-    },
-    cache_enabled = 0,
-}
-
-vim.opt.clipboard = "unnamedplus"
+-- vim.opt.clipboard = "unnamedplus"
 
 -- line numbers and relative line numbers
 vim.opt.number = true
+
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 
+-- Set line number colours
 vim.api.nvim_set_hl(0, "AbsoluteLineNr", {
-	fg = "#6c7086",
+    fg = "#6c7086",
 })
 
 vim.api.nvim_set_hl(0, "RelativeLineNr", {
-	fg = "#45475a",
+    fg = "#2D2E40",
 })
 
 vim.opt.statuscolumn = table.concat({
