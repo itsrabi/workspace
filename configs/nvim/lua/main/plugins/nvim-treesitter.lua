fd2777f04dd42ae81d84e62d2353d14b730e20b1
Rabi Ahmad <rabi.ahmad@capgemini.com>
2026-08-13 16:48:26 +0100

    more nvim configurations

--- configs/nvim/lua/main/plugins/nvim-treesitter.lua
+++ configs/nvim/lua/main/plugins/nvim-treesitter.lua
@@ -1,37 +1,6 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    config = function()
-        local ok, configs = pcall(require, "nvim-treesitter.configs")
-        if not ok then
-            return
-        end
-
-        configs.setup({
-            ensure_installed = {
-                "lua",
-                "vim",
-                "vimdoc",
-                "python",
-                "bash",
-                "markdown",
-                "markdown_inline",
-                "json",
-		"yaml",
-		"html",
-		"javascript",
-		"typescript",
-		"tsx",
-		"css",
-		"dockerfile",
-            },
-            auto_install = true,
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-	    indent = { 
-	        enable = true, 
-	    },
-        })
-    end,
+  'nvim-treesitter/nvim-treesitter',
+  dependencies = { 'neovim-treesitter/treesitter-parser-registry' },
+  lazy = false,
+  build = ':TSUpdate',
 }
